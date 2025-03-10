ffd52ecbe00a3181697ea1de50350595788d98be
Atig-Hamza <hamzaatig@icloud.com>
2025-03-10 13:41:36 +0000

    add all rentalcontroller function and test theme in postman

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'start_date',
+        'end_date',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\RentalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('users', AuthController::class);
 
 Route::apiResource('cars', CarController::class);
+
+Route::apiResource('rentals', RentalController::class);
